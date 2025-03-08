cbbaf48a229df161f7715ed374eafd786c03d0d4
Sreenesh S <143419702+Sreenesh123@users.noreply.github.com>
2025-03-09 00:22:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Project/", // Add this line to match your deployment path
+  // base: "/Project/", // Add this line to match your deployment path
 });
